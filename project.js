dc01c6bf07dce1b5954c45d237df93c4b41c2f56
dianageorge <dianageorge1994@gmail.com>
2021-04-12 00:21:57 +0530

    json structure for reading values

---
+++ project.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/',(req,res)=>{
+    res.send("in default page")
+})
+
+app.post('/add',(req,res)=>{
+    var  x=parseFloat(req.body.a)
+    var y=   parseFloat(req.body.b)
+    var result=a+b
+    res.send(result)
+})
+
+
+app.post('/sum',(req,res)=>{
+    var  x=parseFloat(req.body.a)
+    var y=   parseFloat(req.body.b)
+    var result=x+y
+    res.json({"result": result})
+})
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server started")
+})
